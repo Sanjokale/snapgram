e576a14c8bddcc419f1f58b12894662be8ed665a
Sanjok Alemagar <alemagarsanjok771@gmail.com>
2024-11-15 11:32:02 +0545

    encrypt password before saving the database

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,23 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/snapgram");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 app.use(express.json())
 app.use(cors())
+
+const dbConnect = async()=>{
+  try{
+    const isConnected =  await mongoose.connect('mongodb://127.0.0.1:27017/snapgram');
+    if(isConnected) console.log("connected to mongodb")
+  }catch(err){
+  console.log(err)
+  }
+}
+dbConnect()
+
 
 const { Schema } = mongoose;
 // email, phoneNumber, password, role, fullName, fatherName, motherName)
@@ -23,9 +36,18 @@ const User = mongoose.model("User", userSchema);
 //   password: "1223445",
 // });
 
-app.post("/register", (req, res) => {
-  User.create(req.body);
-  res.send("User created!");
+app.post("/register", async(req, res) => {
+   //1. email exists or not?
+   const emailExist = await User.exists({email: req.body.email})
+   if(emailExist) return res.status(409).send({msg: "Email already exist!"})
+   // yes exists: 
+       //-------> return msg email taken
+   // no exists:
+       //2. password hash
+       req.body.password = await bcrypt.hash(req.body.password, saltRounds);
+       //3. save to db
+       User.create(req.body)
+       res.send({msg: " Account created successfully"})
 
 });
 
