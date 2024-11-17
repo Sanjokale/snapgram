6d2601c79130f8af68a40ec6bd7edebf46f1083c
Sanjok Alemagar <alemagarsanjok771@gmail.com>
2024-11-17 08:07:58 +0545

    "Added dotenv, jsonwebtoken, and updated dependencies in package.json; modified index.js to include dotenv config, jwt, and login endpoint"

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = 8000;
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-const dbConnect = async()=>{
-  try{
-    const isConnected =  await mongoose.connect('mongodb://127.0.0.1:27017/snapgram');
-    if(isConnected) console.log("connected to mongodb")
-  }catch(err){
-  console.log(err)
+const dbConnect = async () => {
+  try {
+    const isConnected = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/snapgram"
+    );
+    if (isConnected) console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
   }
-}
-dbConnect()
+};
+dbConnect();
 
+//console.log(process.env.SECRET_KEY);
 
 const { Schema } = mongoose;
 // email, phoneNumber, password, role, fullName, fatherName, motherName)
@@ -36,19 +40,42 @@ const User = mongoose.model("User", userSchema);
 //   password: "1223445",
 // });
 
-app.post("/register", async(req, res) => {
-   //1. email exists or not?
-   const emailExist = await User.exists({email: req.body.email})
-   if(emailExist) return res.status(409).send({msg: "Email already exist!"})
-   // yes exists: 
-       //-------> return msg email taken
-   // no exists:
-       //2. password hash
-       req.body.password = await bcrypt.hash(req.body.password, saltRounds);
-       //3. save to db
-       User.create(req.body)
-       res.send({msg: " Account created successfully"})
+app.post("/register", async (req, res) => {
+  //1. email exists or not?
+  const emailExist = await User.exists({ email: req.body.email });
+  if (emailExist) return res.status(409).send({ msg: "Email already exist!" });
+  // yes exists:
+  //-------> return msg email taken
+  // no exists:
+  //2. password hash
+  req.body.password = await bcrypt.hash(req.body.password, saltRounds);
+  //3. save to db
+  User.create(req.body);
+  res.send({ msg: " Account created successfully" });
+});
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  //STEP 1: check if email exists
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(401).send({ msg: "Invalid Email!!" });
+
+  //STEP 2: Compare the password
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched)
+    return res.status(401).send({ msg: "Invalid Password!!" });
+
+  //STEP 3: Generate unique token for the user to mark that he is logged in
+  const token = jwt.sign({ email }, process.env.SECRET_KEY);
+
+  res.send({
+    token,
+    user,
+    isLoggednIn: true,
+    msg: "Authorized!!",
+  });
 });
 
 app.get("/users", (req, res) => {
